151fa88032d9bd21aa87b117fccf0db11e1cf710
sunglassman <43784491+sunglassman@users.noreply.github.com>
2018-12-05 12:42:17 -0500

    Add files via upload

---
+++ Lesson6/Problem2/problem2.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+print('Nice to meet you ' + name + ', my name is Chatbot.')
+age = input('How old are you? ')
+print('Wow, ' + age + ' years old! I was born today.')
+feeling = input('How are you feeling today? ')
+print('Glad to hear you are feeling ' + feeling.lower() + '. That is wonderful to hear!')
+day = input('What is your favorite day of the week? ')
+print('Wow! ' + day.title() + ' is my favorite day too!')
---
+++ Lesson6/Problem3/problem3.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+print('UGH, I HATE YOU ' + name.upper() + '.')
+color = input('What is your favorite color? ')
+print('UGH, I HATE ' + color.upper() + ', THERE ARE SO MANY BETTER COLORS.')
+subject = input('What is your favorite subject in school? ')
+print('UGH, I HATE ' + subject.upper() + '. I HATE EVERYTHING!')
+chatbot = input('What is your favorite chatbot? ')
+print('UGH, I HATE ' + chatbot.upper() + "!")
